00af5dc1ef73abd5be91aeb174471d977c7061ea
KairoDocs Developer <dev@kairodocs.local>
2026-04-10 18:32:01 +0530

    chore: move firebase config to environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqZ-VBV6swHBkVNW1Fpu4yx0c6Zf-YIPE",
-  authDomain: "kairodocs-database.firebaseapp.com",
-  databaseURL: "https://kairodocs-database-default-rtdb.firebaseio.com",
-  projectId: "kairodocs-database",
-  storageBucket: "kairodocs-database.firebasestorage.app",
-  messagingSenderId: "808095840055",
-  appId: "1:808095840055:web:3bef883409de15e0d74cb2",
-  measurementId: "G-P9L9NHGNKX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase only once
